b7e12d8622c664cb53d6ca02ad7106e1e267c75b
Codeco <adrielcodeco@hotmail.com>
2023-07-19 17:40:36 -0300

    fix: conformance-suite

--- obb-certification/suite-caller.sh
+++ obb-certification/suite-caller.sh
@@ -2,7 +2,7 @@
 
 java \
     -jar /server/fapi-test-suite.jar \
-    -Djdk.tls.maxHandshakeMessageSize=65536 \
     --fintechlabs.base_url=$BASE_URL \
     --fintechlabs.devmode=true \
-    --fintechlabs.startredir=true
+    --fintechlabs.startredir=true \
+    -Djdk.tls.maxHandshakeMessageSize=65536
