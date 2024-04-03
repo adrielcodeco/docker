10bf4e92c1354901e0f46d02d79fc2460b3f1199
Codeco <adrielcodeco@hotmail.com>
2024-04-03 17:00:59 -0300

    directory urls

--- obb-certification/suite-caller.sh
+++ obb-certification/suite-caller.sh
@@ -3,6 +3,8 @@
 java \
     -jar /server/fapi-test-suite.jar \
     --fintechlabs.base_url=$BASE_URL \
+    --fintechlabs.directoryroots.uri=$DIRECTORYROOTS_URL \
+    --fintechlabs.yacs.directory.uri=$DIRECTORY_PARTICIPANTS_URL \
     --fintechlabs.devmode=true \
     --fintechlabs.startredir=true \
     -Djdk.tls.maxHandshakeMessageSize=65536
